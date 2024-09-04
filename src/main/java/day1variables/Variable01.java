8a06b878a8df66c836f875b0d2c3a16bc21ec2a6
KubraIsik <kuisik@gmail.com>
2024-09-04 13:24:13 +0300

    first commit

---
+++ src/main/java/day1variables/Variable01.java
@@ -0,0 +1,17 @@
+package day1variables;
+
+public class Variable01 {
+
+    public static void main(String[] args){
+        // comment line
+        /*1) Java dili Strongly Typed’dir, bu nedenle Java’daki her değişken(variable), belirli
+                veri türü(data type) ile iliskilendirilmelidir.
+          2) Kod yazanin değişkenlere uygun değerlerin atandığından emin olması her zaman gerekir.
+            Aksi takdirde hata oluşur.*/
+
+        //Example 1: By creating a String data type variable with the name hi,
+        // load "Hello World" into it and write it to the console
+        String hi = "Hello World";
+        System.out.println(hi);
+    }
+}
