6062fcc61bca079b84abb425348f510c29cbf7d1
KubraIsik <kuisik@gmail.com>
2024-09-24 22:07:15 +0300

    day 10 For & While Loops

---
+++ src/main/java/day10loops/ForLoops03.java
@@ -0,0 +1,26 @@
+package day10loops;
+
+public class ForLoops03 {
+    public static void main(String[] args) {
+        /*
+        Example 1: Write the code to give the following output
+                    Week: 1
+                        Day: 1
+                        Day: 2
+                        Day: 3
+                        .....
+                    Week: 2
+                        Day: 1
+                        Day: 2
+                        Day: 3
+                        ....    */
+
+        //nested for
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Week: " + i);
+            for (int j = 1; j < 8; j++) {
+                System.out.println("Day: " + j);
+            }
+        }
+    }
+}
---
+++ src/main/java/day10loops/ForLoops05.java
@@ -0,0 +1,22 @@
+package day10loops;
+
+public class ForLoops05 {
+    public static void main(String[] args) {
+        //Example 1: Find the sum of the digits in the decimal part of the given number.
+        //            28.587 ==> 5+8+7=20
+        double num = 28.587;
+        String sNum = String.valueOf(num); // "28.587"
+        String decimalPart = sNum.split("[\\.]")[1];
+        int intDecimalPart = Integer.valueOf(decimalPart);
+        int sum = 0;
+        for (int i = intDecimalPart; i > 0; i /= 10) {
+            int digit = i % 10;
+            sum += digit;
+            if ( i!= intDecimalPart){
+                System.out.print("+");
+            }
+            System.out.print(digit);
+        }
+        System.out.println(" = " + sum);
+    }
+}
